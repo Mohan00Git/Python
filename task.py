33cff82226b6bc35b07cb78559b1993f665d6a9b
Mohan Kumar <mohankumar04theni@gmail.com>
2025-06-16 19:16:53 +0530

    Day 1

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city_Name = input("What is the name of thr city you grew up in?\n")
+pet_Name = input("What is your pet's name?\n")
+print("Your brand name could be " + city_Name + " " + pet_Name)
