40bd158e71e7c7411550643ef44fb748fe4e5d9e
Mohan Kumar M <mohankumar04theni@gmail.com>
2025-06-16 20:05:24 +0530

    Update task2.py

--- Day2/task2.py
+++ Day2/task2.py
@@ -1,1 +1,7 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $\n"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 \n"))
+people = int(input("How many people to split the bill?\n "))
+calculation = (bill + tip/100*bill)/people
 
+print(f"Each person should pay: {(round(calculation, 2))} ")
